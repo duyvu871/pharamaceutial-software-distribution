25dd89d29db9433e28c5949432705833f2f15b09
duyvu871 <zoocp123@gmail.com>
2025-01-24 13:26:16 +0700

    [todo]: build admin

--- apps/web/src/app/(admin)/admin/login/page.tsx
+++ apps/web/src/app/(admin)/admin/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import AppLayout from '@layout/app-layout.tsx';
 import RefreshCookieAuthProvider from '@provider/refresh-cookie-provider.tsx';
 import AdminLoginForm from '@container/admin/auth/login.tsx';
@@ -9,13 +9,15 @@ import { AdminSidebarLayout } from '@layout/admin-sidebar-layout.tsx';
 function Page() {
 	return (
 		<RefreshCookieAuthProvider>
-			<AppLayout>
-				{/*<AdminSidebarLayout>*/}
-					<AdminLoginForm />
-				{/*</AdminSidebarLayout>*/}
-				{/*<ProfileProvider>*/}
-				{/*</ProfileProvider>*/}
-			</AppLayout>
+			<Suspense>
+				<AppLayout>
+					{/*<AdminSidebarLayout>*/}
+						<AdminLoginForm />
+					{/*</AdminSidebarLayout>*/}
+					{/*<ProfileProvider>*/}
+					{/*</ProfileProvider>*/}
+				</AppLayout>
+			</Suspense>
 		</RefreshCookieAuthProvider>
 	);
 }
